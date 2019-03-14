0bf32b8699c3629e3891db6d340c16f00758fb00
Michelle Hughes <shellj666@gmail.com>
2019-03-14 20:27:34 +0000

    Open Phone Log File
    Opens the saved phone log file in read only mode

--- PhoneLog/PhoneLog/Form1.cs
+++ PhoneLog/PhoneLog/Form1.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace PhoneLog
 {
     public partial class Form_PhoneLog : Form
     {
+        string PhoneLogFile = @"C:\Users\Michelle\Desktop\PhoneLog\PhoneLog\Items\PhoneLog";
+
         public Form_PhoneLog()
         {
             InitializeComponent();
+            Lbl_Name.Text = "Welcome " + SystemInformation.UserName;
         }
 
         private void Btn_Exit_Click(object sender, EventArgs e)
@@ -26,6 +30,13 @@ namespace PhoneLog
         {
             Form_About form_About = new Form_About();
             form_About.Show();
+        }
+
+        private void Btn_OpenPhoneLog_Click(object sender, EventArgs e)
+        {
+            Excel.Application ExcelApp = new Excel.Application();
+            Excel.Workbook ExcelWrkBook = ExcelApp.Workbooks.Open(PhoneLogFile, Excel.XlFileAccess.xlReadOnly, true);
+            ExcelApp.Visible = true;
         }
     }
 }
